ef310ea5da0186da1e54e89d275345fb62e7648a
Matias De Vito <matiasdv1@hotmail.com>
2024-11-19 03:21:29 -0300

    chore: env for origin host added

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,25 +2,39 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"github.com/matidev200/guardaloya-backend/internal/credentials"
 	"github.com/matidev200/guardaloya-backend/internal/database"
 	"github.com/matidev200/guardaloya-backend/internal/login"
 	"github.com/matidev200/guardaloya-backend/internal/users"
 )
+
+func LoadEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("unable to load .env file")
+	}
+}
+
 func enableCORS(router *mux.Router) {
+	FRONTEND_HOST := os.Getenv("FRONTEND_HOST")
 	router.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", FRONTEND_HOST)
 	}).Methods(http.MethodOptions)
 	router.Use(middlewareCors)
 }
 
 func middlewareCors(next http.Handler) http.Handler {
+	FRONTEND_HOST := os.Getenv("FRONTEND_HOST")
+
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, req *http.Request) {
-      w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+      w.Header().Set("Access-Control-Allow-Origin", FRONTEND_HOST)
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,PATCH")
 			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
